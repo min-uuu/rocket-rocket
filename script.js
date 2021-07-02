e33f3b44b9d9e34fa769319bc634840cd4f509e9
min-uuu <minuuu03@gmail.com>
2021-07-02 21:57:52 +0900

    로켓은 슈우우웅

---
+++ script.js
@@ -0,0 +1,33 @@
+let getRocket = () => {
+    fetch(`https://fdo.rocketlaunch.live/json/launches/next/5`)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+
+            var missionName = document.getElementsByClassName("missionName");
+            var provider = document.getElementsByClassName("provider");
+            var country = document.getElementsByClassName("country");
+            var date = document.getElementsByClassName("date");
+            var discription = document.getElementsByClassName("discription");
+
+            for (let i = 0; i < missionName.length; i++) {
+                missionName[i].innerHTML = data["result"][i].name;
+            }
+            for (let i = 0; i < provider.length; i++) {
+                provider[i].innerHTML = data["result"][i].provider.name;
+            }
+            for (let i = 0; i < country.length; i++) {
+                country[i].innerHTML = data["result"][i].pad.location.country
+            }
+            for (let i = 0; i < date.length; i++) {
+                date[i].innerHTML = data["result"][i].date_str;
+            }
+            for (let i = 0; i < discription.length; i++) {
+                discription[i].innerHTML = data["result"][i].launch_description;
+            }
+        })
+}
+
+getRocket();
